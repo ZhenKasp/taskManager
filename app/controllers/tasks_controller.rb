46402f6e83e929b4838ee473f1cb2603643e8e71
Evgeny <zhenkasp@mail.ru>
2021-01-25 19:20:52 +0300

    Refactoring tasks_controller and views

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,42 +6,46 @@ class TasksController < ApplicationController
   end
 
   def show
-    @task = current_user.tasks.find(params[:id])
-
-    redirect_to root_path unless @task
+    redirect_to root_path unless task
   end
 
   def new
-    @task = Task.new
+    build_task
   end
 
   def create
-    @task = current_user.tasks.build(params[:task])
-
-    if @task.save
-      redirect_to @task
+    if build_task.save
+      redirect_to task
     else
       render :new
     end
   end
 
   def edit
-    @task = current_user.tasks.find(params[:id])
+    task
   end
 
   def update
-    @task = current_user.tasks.find(params[:id])
-    if @task.update_attributes(params[:task])
-      redirect_to @task
+    if task.update_attributes(params[:task])
+      redirect_to task
     else
-      render action: "edit"
+      render action: :edit
     end
   end
 
   def destroy
-    @task = current_user.tasks.find(params[:id])
-    if @task.destroy
+    if task.destroy
       redirect_to root_path
     end
+  end
+
+  private
+
+  def task
+    @task ||= Task.find(params[:id])
+  end
+
+  def build_task
+    @task = current_user.tasks.build(params[:task])
   end
 end
